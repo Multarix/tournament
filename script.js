0af95d42aeae1f7356a85573a995d216edff37a7
Multarix <darien2105@gmail.com>
2025-05-31 15:39:22 +1000

    stoof

--- script.js
+++ script.js
@@ -14,24 +14,28 @@ const riftstones = [
 ]
 
 
-const codeText = document.getElementById("code");
+const guild1Name = document.getElementById("guild1Name");
+const guild2Name = document.getElementById("guild2Name");
+const confirmNames = document.getElementById("confirmNames");
+
+const coinTossWinner = document.getElementById("coinTossWinner");
+const coinTossGuild1Name = document.getElementById("coinTossGuild1Name");
+const coinTossGuild2Name = document.getElementById("coinTossGuild2Name");
+
+const pickBan = document.getElementById("pickBan");
+
 const ban1 = document.getElementById("ban1");
 const ban2 = document.getElementById("ban2");
 const ban3 = document.getElementById("ban3");
 const ban4 = document.getElementById("ban4");
 const picked = document.getElementById("picked");
+
+const code = document.getElementById("code");
+const generateCode = document.getElementById("generateCode");
+
+const submit = document.getElementById("submit");
+const clear = document.getElementById("clear");
 const output = document.getElementById("output");
-
-const guild1Name = document.getElementById("guild1Name");
-const guild2Name = document.getElementById("guild2Name");
-const guild1NameLabel = document.getElementById("guild1NameLabel");
-const guild2NameLabel = document.getElementById("guild2NameLabel");
-
-const confirm = document.getElementById("g1");
-const radioCoinToss = document.getElementById("g1");
-const radioPickBan = document.getElementById("pick");
-
-
 
 function addOptions(element){
 	for(const riftstone of riftstones){
@@ -42,13 +46,13 @@ function addOptions(element){
 	}
 }
 
-function generateCode() {
+function generateCodeFn() {
 	let string = ""
 	for(let i = 0; i < 6; i++){
 		string += Math.floor(Math.random() * 10);
 	}
 	
-	codeText.value = string;
+	code.value = string;
 }
 
 addOptions(ban1);
@@ -57,33 +61,56 @@ addOptions(ban3);
 addOptions(ban4);
 addOptions(picked);
 
-// Generate Code
-document.getElementById("generateCode").onclick = generateCode;
+generateCode.onclick = generateCodeFn
 
 // Confirm Guild Names
-document.getElementById("confirm").onclick = () => {
-	guild1NameLabel.innerText = guild1Name.value;
-	guild2NameLabel.innerText = guild2Name.value;
+confirmNames.onclick = () => {
+	coinTossGuild1Name.innerText = guild1Name.value;
+	coinTossGuild2Name.innerText = guild2Name.value;
+	coinTossGuild1Name.value = guild1Name.value;
+	coinTossGuild2Name.value = guild2Name.value;
 }
+
+
+// Clear
+clear.onclick = () => {
+	output.value = "";
+	guild1Name.value = "";
+	guild2Name.value = "";
+	code.value = "";
+	coinTossGuild1Name.innerText = "Guild 1"
+	coinTossGuild2Name.innerText = "Guild 2"
+	coinTossGuild1Name.value = "Guild1"
+	coinTossGuild2Name.value = "Guild2"
+	
+	ban1.value = "select";
+	ban2.value = "select";
+	ban3.value = "select";
+	ban4.value = "select";
+	picked.value = "select";
+	coinTossWinner.value = "Guild1";
+	pickBan.value = "pick";
+}
+
 
 
 // Submit
 document.getElementById("submit").onclick = () => {
-	if(!codeText.value) generateCode();
+	if(!code.value) generateCodeFn();
 	const array = [];
 	array.push("-------------------------------------------------")
 	array.push(`Bans: ${[ban1.value, ban2.value, ban3.value, ban4.value].join(", ")}`);
 	array.push(`Pick: ${picked.value}`);
-	array.push(`Code: ${codeText.value}`);
+	array.push(`Code: ${code.value}`);
 	
-	const coinWinner = (radioCoinToss.checked) ? guild1NameLabel.innerText : guild2NameLabel.innerText;
-	const coinLoser = (!radioCoinToss.checked) ? guild1NameLabel.innerText : guild2NameLabel.innerText;
+	const coinWinner = coinTossWinner.value;
+	const coinLoser = (coinTossWinner.value === coinTossGuild1Name.value) ? coinTossGuild2Name.value : coinTossGuild1Name.value;
 	
-	const pickBan = (radioPickBan.checked) ? "Pick" : "Ban";
-	const banPick = (!radioPickBan.checked) ? "Pick" : "Ban";
+	const winnerChose = pickBan.value
+	const LoserChose = (winnerChose === "pick") ? "ban" : "pick";
 	
 	array.push("");
-	array.push(`${coinWinner} won, Chose ${pickBan}. ${coinLoser} ${banPick}s`);
+	array.push(`${coinWinner} won, Chose ${winnerChose}. ${coinLoser} ${LoserChose}s`);
 	array.push("RED: ");
 	array.push("YELLOW: ")
 	array.push("");
@@ -94,13 +121,3 @@ document.getElementById("submit").onclick = () => {
 }
 
 
-
-// Clear
-document.getElementById("clear").onclick = () => {
-	output.value ="";
-	guild1Name.value = "";
-	guild2Name.value = "";
-	codeText.value = "";
-	guild1NameLabel.innerText = "Guild 1"
-	guild2NameLabel.innerText = "Guild 2"
-}
